354deaf7eda40be75d97347243dc19ac877df5f9
pydovah37 <xterm.conf@gmail.com>
2022-10-26 09:29:05 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+// import CardContainer from "./components/card-container/card-container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //constructor method runs first no matter what
+  constructor() {
+    super();
+    this.state = {
+      searchFieldText: "",
+      monsters: [],
+    };
+  }
+
+  //************** METHODS ***************//
+  //lifecycle method runs after render. 
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }));
+  }
+
+  //method to update text that is input into search
+  onSearchChange = (event) => {
+    const searchFieldText = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchFieldText };
+    });
+  }
+
+  //******** RENDER ***********//
+  //render method runs after constructor, and runs every time the DOM needs updated.
+  render() {
+    const{monsters, searchFieldText} = this.state;
+    const{onSearchChange}            = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchFieldText);
+    });
+
+    // We pass our return into our render()
+    return (
+      <div className="App">
+        <h1 className="app-title">Bastard Monsters</h1>
+        <SearchBox 
+          onChangeHandler ={onSearchChange} 
+          placeholder     ='Search Monsters'
+          className       ='monsters-search-box'
+        />
+          
+        <CardList monsters ={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react"
+import CardContainer from "../card-container/card-container";
+import './card-list.styles.css'
+
+class CardList extends Component {
+  render(){
+    const {monsters} = this.props
+
+    return (
+      <div className='card-list'>
+      {
+        monsters.map((monster) => {
+        return <CardContainer monster={monster}/>
+      })} 
+      </div>
+    )
+  }
+}
+
+export default CardList
